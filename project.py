5f6175769dbd4cb8239c84eb44aca28a4760b0ac
Mostafa Eslami <83770847+MostafaEsl@users.noreply.github.com>
2023-07-04 22:44:34 +0330

    added selenium project for Digikala

--- project.py
+++ project.py
@@ -1,1 +1,23 @@
-print('hello world!')
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+driver_path = r"E:\course\python\chromedriver.exe" 
+
+options = Options()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+
+with webdriver.Chrome(service=Service(driver_path), options=options) as driver:
+    driver.get(url)
+    element = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[4]/div[1]/header/div[2]/a/button')
+    element.click()
+    input_element = driver.find_element(By.XPATH, "/html/body/div[1]/main/div[2]/div[2]/form/label/div/div/input")
+    input_element.send_keys("mostafaesl76@gmail.com")
+    element2 = driver.find_element(By.XPATH, ' /html/body/div[1]/main/div[2]/div[2]/form/button/div[2]')
+    element2.click()
+    input_element1 = driver.find_element(By.XPATH, "/html/body/div[1]/main/div[2]/div[2]/form/label/div/div[1]/input")
+    input_element1.send_keys("10152099Most@f@")
+    element2 = driver.find_element(By.XPATH, ' /html/body/div[1]/main/div[2]/div[2]/button[2]/div[2]')
+    element2.click()
