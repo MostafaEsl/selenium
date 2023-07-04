ecf8ed3e3cf27e56d10cdd7727ad3ae278f770c7
nazi1920 <88589637+nazi1920@users.noreply.github.com>
2023-07-05 01:10:11 +0330

    Update project.py
    added common.key

--- project.py
+++ project.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
+import time
 driver_path = r"E:\course\python\chromedriver.exe" 
 
 options = Options()
