8e548f3f5faffd65445c02707a5f38c0334157ed
nazi1920 <88589637+nazi1920@users.noreply.github.com>
2023-07-05 17:15:45 +0330

    added delay bug .py

--- project.py
+++ project.py
@@ -26,3 +26,8 @@ with webdriver.Chrome(service=Service(driver_path), options=options) as driver:
     element2 = driver.find_element(By.XPATH, ' /html/body/div[1]/main/div[2]/div[2]/button[2]/div[2]')
     element2.click()
     time.sleep(5)
+    element2 = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/header/div[2]/div/div/div[2]/div[1]/div[2]/a/div')
+    element2.click()
+    time.sleep(20)
+
+
